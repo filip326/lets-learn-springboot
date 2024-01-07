e74620033b8b6f8ae025d589a47b3961d97ccb36
heeecker <89743412+filip326@users.noreply.github.com>
2024-01-07 19:18:49 +0100

    first commit

---
+++ src/main/java/me/heeecker/kniffel/KniffelApplication.java
@@ -0,0 +1,13 @@
+package me.heeecker.kniffel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KniffelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KniffelApplication.class, args);
+	}
+
+}
